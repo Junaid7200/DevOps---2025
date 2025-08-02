69de051f1e7041ef1907cd52883d3f1df011dc68
Syed Junaid Jaffery <junaidjaffery1@gmail.com>
2025-08-02 15:36:46 +0500

    modifications made to github actions files and server.js

--- week5/dockerized_app/server.js
+++ week5/dockerized_app/server.js
@@ -1,10 +1,40 @@
 const http = require('http');
-const prom = require('prometheus-api-metrics');
+const promClient = require('prom-client');
 
-prom();
+// Create a Registry to register metrics
+const register = new promClient.Registry();
+
+// Create a counter metric
+const httpRequestsTotal = new promClient.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'status'],
+});
+
+// Register the metric
+register.registerMetric(httpRequestsTotal);
+
+// Add a default metric
+register.setDefaultLabels({
+  app: 'node-app'
+});
+
+// Enable the default metrics
+promClient.collectDefaultMetrics({ register });
+
+// Create HTTP server
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  httpRequestsTotal.inc({ method: req.method, status: 200 });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello from Node.js!\n');
+});
+
+// Expose metrics endpoint
+server.on('request', (req, res) => {
+  if (req.url === '/metrics') {
+    res.setHeader('Content-Type', register.contentType);
+    register.metrics().then(metrics => res.end(metrics));
+  }
 });
 
 server.listen(3000, () => console.log('Server running on port 3000'));
